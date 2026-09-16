c99ed55109c5f53fb169ba33511b5db74cdeeef1
Marvel Zhu <qizhu@microsoft.com>
2026-09-16 14:45:42 +0800

    Fix double-click select all in Windows native text boxes

--- winNativeApp/App.xaml.cs
+++ winNativeApp/App.xaml.cs
@@ -1,7 +1,25 @@
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace HttpDebug;
 
 public partial class App : Application
 {
+	public App()
+	{
+		EventManager.RegisterClassHandler(typeof(TextBox), UIElement.PreviewMouseDownEvent,
+			new MouseButtonEventHandler(TextBox_PreviewMouseDown));
+	}
+
+	private static void TextBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+	{
+		if (e.ChangedButton != MouseButton.Left || e.ClickCount != 2)
+			return;
+
+		var textBox = (TextBox)sender;
+		textBox.Focus();
+		textBox.SelectAll();
+		e.Handled = true;
+	}
 }
